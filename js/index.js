3841b4673f249f9a427a36f9ebd45950c36d6d13
efcwpt <efcwpt2015@gmail.com>
2021-12-11 16:27:17 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+     $( ".item-joselin" ).click(function() {
+        $( ".text-celebrate" ).hide( "drop", { direction: "right" }, "slow" );
+        $( ".text-joselin" ).show( "fade", 1000 );
+      });
+
+
+      
